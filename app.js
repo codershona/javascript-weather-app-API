0129dc79a071b8db60c318c6e0193c79bec83588
codershona <falgunimst95@gmail.com>
2020-03-19 18:08:57 +0100

    openweather API key added

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // function :
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+// var name = document.querySelector('.name');
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=478cc3c9e7cd7db831d6f004db3d70f7')
+   .then()
