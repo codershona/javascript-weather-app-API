d6a3c9ae0b1514278a6bc455c96e6c139cabde9f
codershona <falgunimst95@gmail.com>
2020-03-19 18:20:31 +0100

    some errors fixed and api key weather alert and method is working

--- app.js
+++ app.js
@@ -6,5 +6,17 @@ var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 // var name = document.querySelector('.name');
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=478cc3c9e7cd7db831d6f004db3d70f7')
-   .then()
+// fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=478cc3c9e7cd7db831d6f004db3d70f7')
+//    .then(response => response.json())
+//    .then(data => console.log(data))
+//
+//    .catch(err => alert("Wrong City Name, Please the correct city!"))
+
+
+button.addEventListener('click', function() {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=478cc3c9e7cd7db831d6f004db3d70f7')
+     .then(response => response.json())
+     .then(data => console.log(data))
+
+     .catch(err => alert("Wrong city name, please enter correct city name!"))
+})
